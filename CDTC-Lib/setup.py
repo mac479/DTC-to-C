de87655b8fa439539e80a413536edca95b26916b
mac479 <47116135+mac479@users.noreply.github.com>
2021-05-03 21:51:20 -0400

    Updated library

--- CDTC-Lib/setup.py
+++ CDTC-Lib/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='DTCTC',
     packages=find_packages(),
-    version='1.2.1',
+    version='1.2.2',
     description='Converts a SKLearn DTC model into executable c code. Also can create a test program to compile and test the exported code.',
     author='Myles Conlon'
 )
